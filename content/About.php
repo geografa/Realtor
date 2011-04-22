00e90e9a05c8d0274d02f3f7e90fb96d1aea99bd
geografa <rafamoustafah@gmail.com>
2011-04-22 13:30:56 -0700

    adjusted css for new images.

--- content/About.php
+++ content/About.php
@@ -8,11 +8,33 @@
 
 <div id="wrapper">
 	<div id="content">	
-		<div id="inner-content">	
-			<div class="one">
-				<p><img src="images/karim.jpg" width="240" height="368" alt="Karim" class="imgleft"></p>As a Realtor working in Portland, I have created this site as a resource for buyers & sellers who want to be proactive and well informed about the current market. If you are interested in buying or selling, please <a href="Listings.php">Sign Up To Receive A Daily Listing Email</a>. I look forward to talking with you about houses.
-				<p>Karim & Leonie are parents of 2 boys, SE Portland residents and world travelers... We are very lucky to have family to visit all over the world. We've recently traveled to England, Scotland, Australia, France, Senegal... all to visit close family.</p>
-			</div><!--END one-->
-		</div><!--END inner-content-->
+		<div id="inner-content-full">	
+			
+			<div class="slideshow-container">
+				<div class="slideshow">
+					<div class="nivoSlider" id="slidernivo">
+						<?php
+						$dir = scandir("images/about");
+						$result = count($dir);
+						$i=2;
+						while($i<=$result)
+							{
+								echo "<img src='images/about/$dir[$i]' />";
+								$i++;
+							}
+						?>
+						<!-- <img src="images/slides/slide1.1.jpg" /> -->
+					</div><!--END slidernivo-->
+					
+				</div><!--END slideshow-->
+
+			</div><!--END slideshow-container-->
+
+				<div class="services-list-bottom">
+					<p>Karim & Leonie are parents of 2 boys, SE Portland residents and world travelers... We are very lucky to have family to visit all over the world. We've recently traveled to England, Scotland, Australia, France, Senegal... all to visit close family.</p>
+				</div><!--END services-list-bottom-->
+			
+			
+		</div><!--END inner-content-full-->
 	</div><!--END content-->
 </div><!-- #wrapper -->

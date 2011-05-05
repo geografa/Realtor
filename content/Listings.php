6ccad4f19559dd11c419cd62ec2f3299b2d6d6ed
geografa <rafamoustafah@gmail.com>
2011-05-05 14:00:58 -0700

    updated how-to on the listings page.

--- content/Listings.php
+++ content/Listings.php
@@ -7,11 +7,19 @@
 </div><!--END intro-holder-->
 
 <div id="content">
-	<div id="inner-sidebar" class="right leftborder">
-			<h4>The neighborhood maps are for informational purposes only. Any and all express and implied warranties, including without limitation, warranties of fitness for a particular purpose, warranties of merchantability, and warranties of non-infringement are hereby disclaimed in their entirety. Selliken Systems LLC makes no representations or warranties, express or implied, as to the accuracy or completeness of the information on the HomeQuest site.</h4>
-			<h4>You will start getting these Daily Emails the following day, and you will never be spammed from this source.</h4>
+	<div id="inner-sidebar" class="right leftborder img">
+		<h4>How to Use the Map</h4>
+			<br />
+			<p>Complete the form to the left if you would like to receive current  email listings. You will start getting these Daily Emails the following day. You will never be spammed from this source.</p>
+			<p>Use the <img src="images/zoom.png" width="23" height="23" alt="Zoom"> magnify tool to zoom to a location or neighborhood on the map. Use the <img src="images/select.png" width="23" height="23" alt="Select"> Select tool to select a neighborhood. Slected neighborhoods will appear in the list to the right of the form.</p>
+			<p>Example:</p><img src="images/selected.png" width="200" height="111" alt="Selected">
+			<p>Be sure to enter a number to limit your search criteria. For example, type '300' and '400' to limit the price range from $300,000 to $400,000</p>
+			<p>Example:</p><img src="images/price.png" width="215" height="32" alt="Price">
 	</div><!--END inner-sidebar-->
-	<div id="inner-content">
-		<iframe style="WIDTH: 660px; HEIGHT: 800px" border="0" src="http://www.pdxhomequest.com/map.php?site=1077" frameborder="0" scrolling="no"></iframe>			
+	<div id="inner-content-map">
+		<iframe style="WIDTH: 660px; HEIGHT: 800px" border="0" src="http://www.pdxhomequest.com/map.php?site=1077" frameborder="1" scrolling="no"></iframe>
+		<br />
+		<br />
+		<div>The neighborhood maps are for informational purposes only. Any and all express and implied warranties, including without limitation, warranties of fitness for a particular purpose, warranties of merchantability, and warranties of non-infringement are hereby disclaimed in their entirety. Selliken Systems LLC makes no representations or warranties, express or implied, as to the accuracy or completeness of the information on the HomeQuest site.</div>
 	</div><!--END inner-content-->
 </div><!--END content-->
